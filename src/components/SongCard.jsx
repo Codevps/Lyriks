bd25db3e7f2f20305fcd735b07006dc4e729e999
Pratham Sawant <codevps07@gmail.com>
2022-12-17 20:38:58 +0530

    .now sidebar

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -6,17 +6,17 @@ import PlayPause from "./PlayPause";
 const SongCard = ({ song, i, isPlaying, activeSong, data }) => {
   const dispatch = useDispatch();
   const handlePlayClick = () => {
-    dispatch(playPause(false));
-  };
-  const handlePauseClick = () => {
     dispatch(setActiveSong({ song, data, i }));
     dispatch(playPause(true));
+  };
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
   };
   return (
     <div className="flex flex-col  w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
       <div className="relative w-full h-56 group">
         <div
-          className={`absolute insert-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${
+          className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${
             activeSong?.title === song.title
               ? "flex bg-black bg-opacity-70"
               : "hidden"
