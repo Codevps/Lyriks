e594d5dca1ae7aed9ca8e283c72715c5b18bf9fe
Pratham Sawant <codevps07@gmail.com>
2022-12-19 00:50:15 +0530

    Zod: 2'nd best to God

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -1,5 +1,37 @@
-const RelatedSongs = () => (
-  <div>Loader</div>
-);
+import React from "react";
+import SongBar from "./SongBar";
+
+const RelatedSongs = ({
+  data,
+  isPlaying,
+  activeSong,
+  handlePauseClick,
+  handlePlayClick,
+  artistId,
+  artistName,
+}) => {
+  console.log(data);
+  return (
+    <div className="flex flex-col">
+      <h1 className="font-bold text-3xl text-white">
+        {!artistName ? "Related Songs" : `${artistName}'s Top Songs`}
+      </h1>
+      <div className="mt-6 w-full flex flex-col">
+        {data?.map((song, i) => (
+          <SongBar
+            key={`${song.key}-${artistId}`}
+            song={song}
+            i={i}
+            isPlaying={isPlaying}
+            artistId={artistId}
+            activeSong={activeSong}
+            handlePauseClick={handlePauseClick}
+            handlePlayClick={handlePlayClick}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default RelatedSongs;
--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -1,5 +1,50 @@
-const ArtistDetails = () => (
-  <div>ArtistDetails</div>
-);
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { DetailsHeader, Loader, RelatedSongs, TopPlay } from "../components";
+import { useGetArtistDetailsQuery } from "../redux/services/shazamCore";
+
+const ArtistDetails = () => {
+  const dispatch = useDispatch();
+  const { id: artistId } = useParams();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const {
+    data: artistData,
+    isFetching: isFetchingArtistDetails,
+    error,
+  } = useGetArtistDetailsQuery(artistId);
+
+  let artistSongs;
+  let artistName;
+  let finalData;
+  artistSongs = [artistData?.data[0]?.views];
+  if (isFetchingArtistDetails) {
+    return <Loader title="Loading Artist Details" />;
+  }
+  if (error) {
+    <Error />;
+  }
+  return (
+    <div className="flex flex-col">
+      <DetailsHeader artistId={artistId} artistData={artistData} />
+      <div style={{ display: "none" }}>
+        {Object.values(artistSongs[0]).map((nachos, index) => {
+          <div>
+            {nachos?.attributes?.title === "Top Songs" &&
+              (finalData = nachos?.data)}
+          </div>;
+        })}
+        {console.log(finalData)}
+      </div>
+      <RelatedSongs
+        data={finalData}
+        artistId={artistId}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+        artistName={artistData?.data[0]?.attributes?.name}
+      />
+    </div>
+  );
+};
 
 export default ArtistDetails;
